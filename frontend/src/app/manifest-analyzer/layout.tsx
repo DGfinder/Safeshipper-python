42dbbe76aa117beaae8412955c51dfbf71d773f4
DGFinder <hayden.george.hamilton@gmail.com>
2025-07-02 22:22:46 +0800

    Complete frontend cleanup: remove old frontend, rename frontend-new to frontend

--- frontend/src/app/manifest-analyzer/layout.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from '@/components/DashboardLayout'
-
-export default function ManifestAnalyzerLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <DashboardLayout>{children}</DashboardLayout>
-} 
